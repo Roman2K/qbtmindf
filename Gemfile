c0b1ddaf140fbbd5338541800b8ffd9a2daa239b
Roman Le Négrate <roman.lenegrate@gmail.com>
2019-06-04 15:50:53 +0200

    move QBitTorrent to its own lib

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source 'https://rubygems.org'
 
 gem 'metacli', github: 'Roman2K/metacli'
+gem 'qbittorrent-ruby', github: 'Roman2K/qbittorrent-ruby'
--- main.rb
+++ main.rb
@@ -1,122 +1,18 @@
-require 'net/http'
-require 'json'
-
-def df(path, block_size)
-  IO.popen(["df", "-B#{block_size}", path], &:read).
-    tap { $?.success? or raise "df failed" }.
-    split("\n").
-    tap { |ls| ls.size == 2 or raise "unexpected number of lines" }.
-    fetch(1).
-    split(/\s+/).
-    fetch(-3).
-    chomp(block_size).to_f
-end
-
-class QbtClient
-  def initialize(uri)
-    user, password = uri.user, uri.password
-    @uri = uri.dup.tap { |u| u.user = u.password = nil }.freeze
-    set_cookie! user, password
-  end
-
-  def download_limit=(n)
-    $stderr.puts "DEBUG setting download limit to: %d bytes" % n
-    post! "/command/setGlobalDlLimit", 'limit' => n
-  end
-
-  def pause_downloading
-    downloading.each { |t| pause t unless t.fetch("state") == "pausedDL" }
-  end
-
-  def resume_downloading
-    downloading.each { |t| resume t if t.fetch("state") == "pausedDL" }
-  end
-
-  private def downloading
-    JSON.parse(get!("/query/torrents?filter=downloading").body).tap do |dls|
-      $stderr.puts "DEBUG fetched %d downloading torrents" % dls.size
-    end
-  end
-
-  private def pause(t)
-    $stderr.puts "DEBUG pausing torrent: %s" % t.fetch("name")
-    post_hash! "/command/pause", t
-  end
-
-  private def resume(t)
-    $stderr.puts "DEBUG resuming torrent: %s" % t.fetch("name")
-    post_hash! "/command/resume", t
-  end
-
-  private def post_hash!(path, t)
-    post! path, 'hash' => t.fetch("hash")
-  end
-
-  private def get!(path)
-    request! new_req(:Get, path)
-  end
-
-  private def post!(path, data)
-    req = new_req :Post, path
-    req.form_data = data
-    request! req
-  end
-
-  private def set_cookie!(user, password)
-    req = new_req :Post, "/login"
-    req.form_data = {'username' => user, 'password' => password}
-
-    @cookie = request!(req)['set-cookie'].
-      tap { |s| s or raise "failed login: %s" % res.body }.
-      split(";", 2).
-      fetch(0)
-  end
-
-  private def new_req(type, path, *args, &block)
-    Net::HTTP.const_get(type).new(add_uri(path), *args, &block).tap do |req|
-      req['Referer'] = @uri.to_s
-      req['Cookie'] = @cookie
-    end
-  end
-
-  private def add_uri(path)
-    b = URI path
-    build_uri do |a|
-      a.path += b.path
-      a.query = [a.query, b.query].compact * "&"
-    end
-  end
-
-  private def build_uri(&block)
-    @uri.dup.tap &block
-  end
-
-  private def request(*args, &block)
-    Net::HTTP.
-      start(@uri.host, @uri.port, use_ssl: @uri.scheme == 'https') do |http|
-        http.request *args, &block
-      end
-  end
-
-  private def request!(*args, &block)
-    request(*args, &block).tap do |res|
-      res.kind_of? Net::HTTPSuccess or raise "unpexpected response: %p" % res
-    end
-  end
-end
+require 'qbittorrent'
 
 module Commands
   def self.cmd_check_min(mnt, min, block_size, qbt_url)
     min = min.to_f
-    avail = df mnt, block_size
     qbt = begin
-      Timeout.timeout(2) do
-        QbtClient.new URI(qbt_url)
+      Timeout.timeout 2 do
+        QBitTorrent.new URI(qbt_url)
       end
     rescue Timeout::Error
       $stderr.puts " WARN qBitTorrent HTTP API seems unavailable, aborting"
       exit 0
     end
+    avail = df mnt, block_size
+
     is_lower = avail < min
     $stderr.puts " INFO available (%d%s) %s minimum (%d%s)" \
       % [avail, block_size, is_lower ? "<" : ">=", min, block_size]
@@ -130,6 +26,17 @@ module Commands
       qbt.download_limit = 0
       qbt.resume_downloading
     end
+  end
+
+  def self.df(path, block_size)
+    IO.popen(["df", "-B#{block_size}", path], &:read).
+      tap { $?.success? or raise "df failed" }.
+      split("\n").
+      tap { |ls| ls.size == 2 or raise "unexpected number of lines" }.
+      fetch(1).
+      split(/\s+/).
+      fetch(-3).
+      chomp(block_size).to_f
   end
 end
 
