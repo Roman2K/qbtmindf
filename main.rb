c2e784c8ade3e5b9adb755591c1bdbf7ee9f8715
Roman Le Négrate <roman.lenegrate@gmail.com>
2020-04-16 12:48:01 +0200

    update utils

--- main.rb
+++ main.rb
@@ -1,27 +1,32 @@
 require 'utils'
 
-module Commands
-  def self.cmd_check_min(mnt, min, block_size, qbt_url)
-    log = Utils::Log.new
+class Commands
+  def initialize(log:)
+    @log = log
+  end
+
+  def cmd_check_min(mnt, min, block_size, qbt_url)
     min = min.to_f
     qbt = begin
-      Utils::QBitTorrent.new URI(qbt_url)
+      Utils::QBitTorrent.new URI(qbt_url), log: @log
     rescue => err
       Utils.is_unavail?(err) or raise
-      log.debug "qBitTorrent HTTP API seems unavailable, aborting"
+      @log.debug "qBitTorrent HTTP API seems unavailable, aborting"
       exit 0
     end
 
     avail = Utils.df mnt, block_size
     is_lower = avail < min
-    log.info "available (%d%s) %s minimum (%d%s)" \
+    @log.info "available (%d%s) %s minimum (%d%s)" \
       % [avail, block_size, is_lower ? "<" : ">=", min, block_size]
 
     if is_lower
-      log.info "pausing downloading torrents"
+      @log.info "pausing downloading torrents"
+      qbt.download_limit = 1024
       qbt.pause_downloading
     else
-      log.info "resuming downloading torrents"
+      @log.info "resuming downloading torrents"
+      qbt.download_limit = 0
       qbt.resume_downloading
     end
   end
@@ -29,5 +34,6 @@ end
 
 if $0 == __FILE__
   require 'metacli'
-  MetaCLI.new(ARGV).run Commands
+  log = Utils::Log.new level: ENV["DEBUG"] == "1" ? :debug : :info
+  MetaCLI.new(ARGV).run Commands.new(log: log)
 end
