f1b25bd8be260b5a94379089c8fbaecad5ee01a2
Roman Le Négrate <roman.lenegrate@gmail.com>
2020-04-29 09:58:13 +0200

    stop limiting DL speeds (also indirectly affects UL speed)

--- main.rb
+++ main.rb
@@ -22,11 +22,9 @@ class Commands
 
     if is_lower
       @log.info "pausing downloading torrents"
-      qbt.download_limit = 1024
       qbt.pause_downloading
     else
       @log.info "resuming downloading torrents"
-      qbt.download_limit = 0
       qbt.resume_downloading
     end
   end
